f02963589f3cb493b614c64d1bffcdc53f6d9639
Christopher Musselle <cmusselle@mango-solutions.com>
2017-06-27 00:08:44 +0100

    complte add templatecommand

--- package/src/stonemason/template.py
+++ package/src/stonemason/template.py
@@ -26,7 +26,7 @@ def initialise_project(project, template=None, output_dir='.'):
                                 node_list=template_names)
 
     # Resolve dependencies for specified template
-    template_order = [n.name for n in resolve(g['package'])]
+    template_order = [n.name for n in resolve(g[template])]
     print(f'Creating project from templates:\n\t{template_order}')
 
     # Initialise output structure to save state of project
@@ -47,7 +47,7 @@ def initialise_project(project, template=None, output_dir='.'):
 
         # Save state
         project_state['variables'].update(content)
-        project_state['templates'].append(template)
+        project_state['templates'].append(name)
 
     # Save state of project variables
     mason_vars = Path(project_dir) / '.mason.json'
@@ -55,17 +55,54 @@ def initialise_project(project, template=None, output_dir='.'):
         json.dump(project_state, f)
 
 
-def add_template(template, project_dir):
+def add_template(template, project_dir, output_dir='.'):
     """ Add a template to an existing project """
 
     # Load existing state information
     mason_vars = Path(project_dir) / '.mason.json'
     with mason_vars.open('r') as f:
         project_state = json.load(f)
-    
-    # TODO: use project_state to:
-    #   Find other template options
-    #   Validate choice of template 
-    #   Work out dependencies 
-    #   Apply new templates in correct order
-    #   Resave project state
+
+    project_templates_root = Path(project_state['variables']['_template']).parent
+
+    previous_templates = project_state['templates']
+
+    paths = project_templates_root.iterdir()
+
+    remaining_templates_paths = {
+        p.name: p for p in paths if p.is_dir() and p.name not in previous_templates
+    }
+
+    assert template in remaining_templates_paths
+
+    # Work out all template names
+    remaining_templates_names = list(remaining_templates_paths.keys())
+
+    # Create graph of template dependencies
+    g = create_dependency_graph(project_templates_root / 'metadata.json',
+                                node_list=remaining_templates_names)
+
+    print(g)
+
+    # Resolve dependencies for specified template
+    template_order = [n.name for n in resolve(g[template])]
+    print(f'Adding the following templates to project:\n\t{template_order}')
+
+    # Cycle through templates and render them
+    for name in template_order:
+        template = remaining_templates_paths[name].as_posix()
+        project_dir, content = render_cookiecutter(
+            template,
+            extra_context=project_state['variables'],
+            output_dir=project_dir, overwrite_if_exists=True,
+        )
+
+        print(f'Rendered: {template}')
+
+        # Save state
+        project_state['variables'].update(content)
+        project_state['templates'].append(name)
+
+    # Save state of project variables
+    with mason_vars.open('w') as f:
+        json.dump(project_state, f)
