38c4f86b4ab30a0bbcf423a1aebe736da8e61aa2
Christopher Musselle <cmusselle@mango-solutions.com>
2017-07-24 19:02:09 +0100

    add tests for 'add' command

---
+++ package/tests/test_add_command.py
@@ -0,0 +1,76 @@
+
+from pathlib import Path
+import pytest
+import git
+import json
+import os
+
+from conftest import TEST_DIR
+
+
+@pytest.fixture(scope='module')
+def init_base_project(tmpdir_factory):
+
+    # Setup a basic project 
+    temp_output_path = tmpdir_factory.mktemp('base_package').strpath
+    template_path = os.path.join(TEST_DIR, 'data', 'python-project')
+
+    # Set arguments
+    args = f"init -o {temp_output_path} {template_path}"
+
+    from stonemason import main
+    # Run from entry point
+    main.main(args=args)
+
+    cookiecutter_vars_path = os.path.join(template_path, "package", "cookiecutter.json")
+    with open(cookiecutter_vars_path, 'r') as f:
+        cookiecutter_vars = json.load(f)
+
+    package_name = cookiecutter_vars['package_name']
+    project_dir = os.path.join(temp_output_path, package_name)
+
+    return project_dir
+
+
+def test_add_works_with_initialised_project(init_base_project):
+
+    current_project_path = Path(init_base_project)
+
+    # Set arguments
+    args = f"add -o {current_project_path.as_posix()} pytest"
+
+    from stonemason import main
+    # Run from entry point
+    main.main(args=args)
+
+    # Check files were created
+    files = [
+        '.git/',
+        '.mason',
+        'MANIFEST.in',
+        'README',
+        'requirements.txt',
+        'setup.py',
+        'src/testpackage',
+        'src/testpackage/__init__.py',
+        'src/testpackage/main.py',
+        'tests/test_foo.py'
+    ]
+    for f in files:
+        p = current_project_path / f
+        assert p.exists()
+
+    # Check requirements were polulated
+    target = "requests\nlogzero\npytest\npytest-cov\ncoverage\n"
+    req_file = current_project_path / 'requirements.txt'
+    text = req_file.read_text()
+
+    assert text == target
+
+    # Check git repo was created and commits made
+    repo_dir = current_project_path 
+    r = git.Repo(repo_dir.as_posix())
+    log = r.git.log(pretty='oneline').split('\n')
+    assert len(log) == 2
+    assert "Add 'pytest' template layer via stone mason." in log[0]
+    assert "Add 'package' template layer via stone mason." in log[1]
