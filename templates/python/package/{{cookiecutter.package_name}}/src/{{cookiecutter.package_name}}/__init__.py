05155947f7560afbd68c7ba00c3d755b59ee80ce
Christopher Musselle <cmusselle@mango-solutions.com>
2017-07-24 23:53:28 +0100

    Remove duplication of basic python template as included in tests

--- templates/python/package/{{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++
@@ -1,5 +0,0 @@
-
-import pathlib
-
-# Store location of where the package is installed
-PKG_DIR = pathlib.Path(__file__).parent
--- templates/python/pytest/hooks/post_gen_project.py
+++
@@ -1,9 +0,0 @@
-""" Hooks to create a new conda environment with the latest version of common libraries 
-
-Also optionally create a git repo and commit the project contents
-"""
-
-from stonemason.utils import postfix
-
-postfix('requirements.txt', 'requirements_postfix.txt')
-
