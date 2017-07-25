bf13261740b9ab3f6357cf73b6d3763927ee9a07
Christopher Musselle <cmusselle@mango-solutions.com>
2017-07-26 00:36:49 +0100

    moved post and prefix functionto to postprocessing

--- package/src/stonemason/utils.py
+++ package/src/stonemason/utils.py
@@ -1,28 +1,8 @@
 
-
 import os 
 from pathlib import Path
 
 import json
-
-
-def postfix(original_file, new_file):
-    """ Add the content of new_content to the end of original_file and remove new_content """
-
-    with open(original_file, 'a') as fout:
-        with open(new_file) as fin:
-            fout.write(fin.read())
-    os.remove(fin.name)
-
-
-def prefix(original_file, new_file):
-    """ Add the content of new_content to the end of original_file and remove new_content """
-
-    with open(new_file, 'a') as fout:
-        with open(original_file) as fin:
-            fout.write(fin.read())
-    os.remove(fin.name)
-    os.rename(fout.name, fin.name)
 
 
 def load_application_data(location=None):
@@ -51,7 +31,7 @@ def save_application_data(obj, location=None):
 
     app_data_dir = Path(os.path.join(location, ".stonemason"))
     app_data_dir.mkdir(exist_ok=True)
-    
+
     template_metadata_path = app_data_dir / "templates.json"
 
     json.dump(obj, template_metadata_path.open('w'))
