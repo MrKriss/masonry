e3df557c842cfb67983306670aebde5b2b254996
Christopher Musselle <cmusselle@mango-solutions.com>
2017-09-28 16:03:34 +0100

    Update conda-recipe template

--- templates/python-ds/conda-recipe/hooks/post_gen_project.py
+++ templates/python-ds/conda-recipe/hooks/post_gen_project.py
@@ -1,14 +1,17 @@
 """ Hook to add all items in the dev_environment file to the conda meat.ymal run requirements """
 
-import pathlib
+import re
+import textwrap
+from pathlib import Path
+
 from ruamel.yaml import YAML
 
 PY_VERSON = "python={{cookiecutter.python_version}}"
 
 # Define Paths
-environment_file = pathlib.Path('.').resolve() / 'package' / 'dev_environment.yml'
+environment_file = Path('.').resolve() / 'package' / 'dev_environment.yml'
 conda_metadata_file = (
-    pathlib.Path('.').resolve() / 'package' / 'recipes' /
+    Path('.').resolve() / 'package' / 'recipes' /
     '{{cookiecutter.package_name}}' / 'meta.yaml'
 )
 
@@ -28,3 +31,27 @@ for package in environment_spec['dependencies']:
 
 # Write out
 yaml.dump(conda_recipe, conda_metadata_file.open(mode='w'))
+
+
+# Comment out dependencies in the setup.py file
+
+def comment_out_setup_dependencies(setup_file_path):
+
+    setup_file_path = Path(setup_file_path)
+    pattern = re.compile(r" *install_requires\s*=\s*\[.*?\]", flags=re.DOTALL)
+
+    # Match the install_requires section
+    res = pattern.search(setup_file_path.read_text())
+
+    # Extract the matched substring and comment out every line
+    new_string = res.group()
+    new_string = textwrap.dedent(new_string)
+    new_string = textwrap.indent(new_string, prefix='# ')
+    new_string = textwrap.indent(new_string, prefix='    ')
+
+    # Substitute new substring into position in the file
+    new_text = pattern.sub(new_string, res.string)
+    setup_file_path.write_text(new_text)
+
+
+comment_out_setup_dependencies('package/setup.py')
