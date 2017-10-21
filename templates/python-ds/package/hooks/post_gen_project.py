02dfb2462a429783c0309eaa58ba8968b9ce8256
Christopher Musselle <cmusselle@mango-solutions.com>
2017-10-21 21:03:28 +0100

    Refactor post hook for 'project' template layer

--- templates/python-ds/package/hooks/post_gen_project.py
+++ templates/python-ds/package/hooks/post_gen_project.py
@@ -9,16 +9,21 @@ Move dev/frozen environment files to package directory
 from pathlib import Path
 import shutil
 
-{% if cookiecutter.transfer_analysis_scripts_to_package == "Yes" %}
-scripts_dir = Path('.').absolute() / 'analysis'
-n = 0
-for file_ in scripts_dir.glob('*.py'):
-    shutil.move(file_.as_posix(), './package/src/{{cookiecutter.package_name}}')
-    n += 1
-print('Moved %d .py files to package/src/{{cookiecutter.package_name}}' % n)
-{% endif %}
+from clint.textui import indent, colored, puts
 
-# Move environment files
-environment_files = Path('.').absolute().glob('*environment.yml')
-for file_ in environment_files:
-    shutil.move(file_.as_posix(), './package/')
+TRANSFER_SCRIPTS = "{{cookiecutter.transfer_analysis_scripts_to_package}}" == 'Yes'
+
+with indent(4):
+    if TRANSFER_SCRIPTS:
+        scripts_dir = Path('.').absolute() / 'analysis'
+        n = 0
+        for file_ in scripts_dir.glob('*.py'):
+            shutil.move(file_.as_posix(), './package/src/{{cookiecutter.package_name}}')
+            n += 1
+        puts('Moved %d .py files to package/src/{{cookiecutter.package_name}}' % n)
+
+    puts('Move environment files to package/')
+    environment_files = Path('.').absolute().glob('*environment.yml')
+
+    for file_ in environment_files:
+        shutil.move(file_.as_posix(), './package/')
