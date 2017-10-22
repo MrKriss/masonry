233dfb9ffbb55f48a2e09e75baeca9cbd2cad5f3
Christopher Musselle <cmusselle@mango-solutions.com>
2017-10-22 22:10:23 +0100

    Add hook to update env with pytest and friends

---
+++ templates/python-ds/pytest/hooks/post_gen_project.py
@@ -0,0 +1,20 @@
+""" Post project creation hooks:
+
+Install new libraries according to dev_environment.yml
+
+"""
+
+from pathlib import Path
+import shutil
+
+from stonemason.utils import run_and_capture
+from clint.textui import indent, colored, puts
+
+
+PROJECT_NAME = "{{cookiecutter.project_name|lower|replace(' ', '_')}}"
+
+conda_update_cmd = f"conda env update -n {PROJECT_NAME} -f dev_environment.yml"
+
+with indent(4):
+    puts("Updating dependencies from new  dev_environment.yml")
+    p = run_and_capture(conda_update_cmd)
