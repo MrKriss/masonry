b46fd250c5763cfa15b57e96896ed3318bac96bc
Christopher Musselle <cmusselle@mango-solutions.com>
2017-10-24 19:53:25 +0100

    Refactor setup.py to use upload command

--- package/setup.py
+++ package/setup.py
@@ -1,24 +1,107 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'masonry'
+DESCRIPTION = 'A command line tool for composable project templating.'
+URL = 'https://github.com/MrKriss/masonry'
+EMAIL = 'chris.j.musselle@gmail.com'
+AUTHOR = 'Chris Musselle'
+
+REQUIRED = [
+    'cookiecutter',
+    'docopt',
+    'schema',
+    'inquirer',
+    'ruamel.yaml',
+    'gitpython',
+    'clint'
+]
+
+repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(repo_root)
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(repo_root, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(repo_root, 'package', 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
 
 setup(
-    name='masonry',
-    description='A command line tool for composable project templating',
-    author='Chris Musselle',
-    author_email='chris.j.musselle@gmail.com',
-    # Required for packaging source distribution from 'src' subfolder
+    # Metadata
+    name=NAME,
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    # Use version control to define version tag
+    use_scm_version={"root": "..", "relative_to": __file__},
+    # Package Locations
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    # This next section registers 'mycommand' as a command line utility that will execute
-    # the `command_line_entry_point()` function in the `command_line` module of `mypackage`
+    # Dependencies
+    install_requires=REQUIRED,
+    include_package_data=True,
+
+    # Entry Point for the command line application
     entry_points={
-        'console_scripts': [
-            'mason=masonry.main:main'
-        ]
+        'console_scripts': ['mason=masonry.main:main']
     },
-    # include_package_data=True,
-    package_data={'rss_miner': ['example_config.yml']},
-    # Use version control to define version tag
-    use_scm_version={"root": "..", "relative_to": __file__}
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+    ],
+    # Command for setup.py publishing support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
