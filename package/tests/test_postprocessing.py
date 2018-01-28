542e480196a2fc5a564cfaea07406f300e7674db
cmusselle <cmusselle@mango-solutions.com>
2018-01-28 21:30:43 +0000

    Add class for postprocessinf pre and post fixes

---
+++ package/tests/test_postprocessing.py
@@ -0,0 +1,73 @@
+
+import pytest
+from pathlib import Path
+import shutil
+import itertools
+
+from conftest import TEST_DIR
+
+from masonry.objects.postprocessors import CombineFileSnippetsPostprocessor
+
+
+def test_combine_file_snippets_postprocessor_postfix(tmpdir):
+
+    # GIVEN a directory of files, with *_post and *_prefixes that match other named files.
+    input_files = [
+        TEST_DIR / Path('data/postprocessing_examples/input/config.ini'),
+        TEST_DIR / Path('data/postprocessing_examples/input/config_postfix.ini')
+    ]
+    # Use a temporary directory for the processing
+    dest_path = Path(tmpdir.strpath) / 'postprocessing'
+    dest_path.mkdir()
+    for fp in input_files:
+        shutil.copy(str(fp), str(dest_path))
+
+    # WHEN combining files using the postprocessing rules
+    postprocessor = CombineFileSnippetsPostprocessor()
+    postprocessor.apply(tmpdir.strpath)
+
+    # THEN config_prefix.ini should be prefixed to config.ini
+    target_path = TEST_DIR / Path('data/postprocessing_examples/output/result_config_postfix.ini')
+    target = target_path.read_text()
+    result_path = dest_path / 'config.ini'
+    result = result_path.read_text()
+    assert target == result
+
+    # THEN *_prefix and *_postfix files hould be removed as part of the processing
+    config_file_present = (result_path / 'config_postfix.ini').exists()
+    assert not config_file_present
+
+    # CLEANUP
+    shutil.rmtree(str(dest_path))
+
+
+def test_combine_file_snippets_postprocessor_prefix(tmpdir):
+
+    # GIVEN a directory of files, with *_post and *_prefixes that match other named files.
+    input_files = [
+        TEST_DIR / Path('data/postprocessing_examples/input/config.ini'),
+        TEST_DIR / Path('data/postprocessing_examples/input/config_prefix.ini'),
+    ]
+    # Use a temporary directory for the processing
+    dest_path = Path(tmpdir.strpath) / 'postprocessing'
+    dest_path.mkdir()
+    for fp in input_files:
+        shutil.copy(str(fp), str(dest_path))
+
+    # WHEN combining files using the postprocessing rules
+    postprocessor = CombineFileSnippetsPostprocessor()
+    postprocessor.apply(tmpdir.strpath)
+
+    # THEN config_prefix.ini should be prefixed to config.ini
+    target_path = TEST_DIR / Path('data/postprocessing_examples/output/result_config_prefix.ini')
+    target = target_path.read_text()
+    result_path = dest_path / 'config.ini'
+    result = result_path.read_text()
+    assert target == result
+
+    # THEN *_prefix and *_postfix files hould be removed as part of the processing
+    config_file_present = (result_path / 'config_prefix.py').exists()
+    assert not config_file_present
+
+    # CLEANUP
+    shutil.rmtree(str(dest_path))
