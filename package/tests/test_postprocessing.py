6b1dccfb764bcfe6a9b55c8f4b84ba23537297b6
cmusselle <cmusselle@mango-solutions.com>
2018-01-30 18:43:56 +0000

    Add tests for code post snd prefixing

--- package/tests/test_postprocessing.py
+++ package/tests/test_postprocessing.py
@@ -6,7 +6,11 @@ import itertools
 
 from conftest import TEST_DIR
 
-from masonry.objects.postprocessors import CombinePostfixFileSnippets, CombinePrefixFileSnippets
+from masonry.objects.postprocessors import (
+    CombineFilePrefix,
+    CombineFilePostfix,
+    CombineCodePrefix
+)
 
 
 def test_combine_file_snippets_postprocessor_postfix(tmpdir):
@@ -23,7 +27,7 @@ def test_combine_file_snippets_postprocessor_postfix(tmpdir):
         shutil.copy(str(fp), str(dest_path))
 
     # WHEN combining files using the postprocessing rules
-    postprocessor = CombinePostfixFileSnippets(pattern='_postfix')
+    postprocessor = CombineFilePostfix(pattern='_postfix')
     postprocessor.apply(dest_path)
 
     # THEN config_prefix.ini should be prefixed to config.ini
@@ -56,7 +60,7 @@ def test_combine_file_snippets_postprocessor_prefix(tmpdir):
         shutil.copy(str(fp), str(dest_path))
 
     # WHEN combining files using the postprocessing rules
-    postprocessor = CombinePrefixFileSnippets(pattern='_prefix')
+    postprocessor = CombineFilePrefix(pattern='_prefix')
     postprocessor.apply(dest_path)
 
     # THEN config_prefix.ini should be prefixed to config.ini
@@ -72,3 +76,48 @@ def test_combine_file_snippets_postprocessor_prefix(tmpdir):
 
     # CLEANUP
     shutil.rmtree(str(dest_path))
+
+
+ns = range(1, 6)
+
+
+@pytest.mark.parametrize('n', ns)
+def test_code_insertion(tmpdir, n):
+
+    # GIVEN original python code file and some new code to insert into it
+    starting_filepath = TEST_DIR / Path(f'data/postprocessing_examples/input/test_code{n}.py')
+    new_content_filepath = TEST_DIR / Path(f'data/postprocessing_examples/input/new_code_prefix.py')
+
+    # Use a temporary directory for the processing
+    dest_path = Path(tmpdir.strpath) / 'postprocessing'
+    dest_path.mkdir()
+    for fp in [starting_filepath, new_content_filepath]:
+        shutil.copy(str(fp), str(dest_path))
+
+    # Rename so file pattern will match
+    src = dest_path / new_content_filepath.name
+    dest = src.with_name(starting_filepath.stem + '_prefix.py')
+    shutil.move(str(src), str(dest))
+
+    # WHEN code is inserted
+    postprocessor = CombineCodePrefix(pattern='_prefix')
+    changed_files = postprocessor.apply(dest_path)
+
+    assert changed_files
+
+    # THEN result should have the code in the right place, with pre and post being before or after
+    # the main body of the code, which is the part after inports and constants, but before the
+    # if __name__ == __main__ clause, if present
+    # target = target_path.read_text()
+    # assert target == result
+
+    # # THEN config_prefix.ini should be prefixed to config.ini
+    # target_path = TEST_DIR / Path('data/postprocessing_examples/output/result_config_prefix.ini')
+    # target = target_path.read_text()
+    # result_path = dest_path / 'config.ini'
+    # result = result_path.read_text()
+    # assert target == result
+
+    # # THEN *_prefix and *_postfix files hould be removed as part of the processing
+    # config_file_present = (result_path / 'config_prefix.py').exists()
+    # assert not config_file_present
