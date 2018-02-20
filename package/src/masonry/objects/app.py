56aac38a1822a6204aeab90b5e0e9e1e932fd9a1
cmusselle <cmusselle@mango-solutions.com>
2018-02-20 23:07:55 +0000

    Split out CLI into top tel App class and CLI as a component of it

---
+++ package/src/masonry/objects/app.py
@@ -0,0 +1,128 @@
+import json
+
+import inquirer
+from pathlib import Path
+
+from .cli import CLI
+from .project import Project
+
+
+class App:
+
+    def __init__(self, args, config=None):
+
+        if not config:
+            self.config = {
+                "application_data": "~/.masonry/",
+                "known_projects": {}
+            }
+        else:
+            self.config = config
+
+        app_data_dir = Path(self.config['application_data']).expanduser().resolve()
+        if not app_data_dir.exists():
+            app_data_dir.mkdir()
+
+        self.app_data_dir = app_data_dir
+
+        self.cli = CLI(args)
+        self.args = self.cli._validate_args()
+
+    def run(self):
+
+        if self.args['init']:
+            self.init_command()
+
+        elif self.args['add']:
+            self.add_command()
+
+    def init_command(self, default_project=None):
+
+        if not self.args['PROJECT']:
+            self.args['PROJECT'] = self._prompt_init_project(default_project)
+
+        project = Project(template_dir=self.args['PROJECT'])
+        project.initialise(output_dir=self.args['--output'], variables={})
+        self.project = project
+
+        self._update_known_projects(project)
+
+    def add_command(self, default_template=None):
+
+        project_dir = Path(self.args['--output']).resolve()
+        mason_file = project_dir / '.mason'
+
+        if not mason_file.exists():
+            raise IOError(
+                f'A ".mason" file was not detected in the output directoty {project_dir}')
+
+        project = Project(mason_file=mason_file)
+
+        if not self.args['TEMPLATE']:
+            # inquire which template to use
+            self.args['TEMPLATE'] = self._prompt_add_template(project, default_template)
+
+        for template_name in self.args['TEMPLATE']:
+            project.add_template(template_name, variables={})
+
+        self.project = project
+
+    def _prompt_init_project(self, default=None):
+        """Return the choice of project to intialise"""
+
+        known_projects = list(self.config['known_projects'].keys())
+        known_projects.sort()
+
+        if not known_projects:
+            raise ValueError("No project specified and no recent projects to choose from.")
+
+        if default:
+            default_idx = known_projects.index(default)
+        else:
+            default_idx = 0
+
+        questions = [
+            inquirer.List('project',
+                          message="What project do you want to initialise?",
+                          choices=known_projects,
+                          default=default_idx)
+        ]
+        answers = inquirer.prompt(questions)
+
+        project_path = known_projects[answers['project']]
+
+        return project_path
+
+    def _prompt_add_template(self, project, default=None):
+        """Return the choice of templates to add"""
+
+        remaining_templates_names = project.remaining_templates
+
+        if default:
+            default_idx = known_projects.index(default)
+        else:
+            default_idx = 0
+
+        questions = [
+            inquirer.Checkbox('templates',
+                              message="What templates do you wish to add?",
+                              choices=remaining_templates_names,
+                              default=default_idx)
+        ]
+
+        answers = inquirer.prompt(questions)
+
+        templates = answers['templates']
+
+        return templates
+
+    def _update_known_projects(self, project):
+
+        project_name = project.template_directory.parent.name
+        self.config['known_projects'][project_name] = str(project.template_directory)
+
+        # Write to output
+        known_projects_path = self.app_data_dir / 'known_projects.json'
+
+        with known_projects_path.open('w', encoding='utf-8') as f:
+            data = json.dump(self.config['known_projects'], f)
--- package/src/masonry/objects/cli.py
+++ package/src/masonry/objects/cli.py
@@ -37,8 +37,6 @@ from .. import __version__
 
 from schema import Schema, And, Or, Use, Optional, SchemaError
 
-from .project import Project
-
 
 class CLI:
 
@@ -46,105 +44,9 @@ class CLI:
         self.args = docopt(__doc__, version=__version__, argv=args)
         self.args = MasonrySchema(self.args).validate()
 
-        if not config:
-            self.config = {
-                "application_data": "~/.masonry/",
-                "known_projects": {}
-            }
-        else:
-            self.config = config
-
     def _validate_args(self):
         args = MasonrySchema(self.args).validate()
         return args
-
-    def run(self):
-
-        self.args = self._validate_args()
-
-        if self.args['init']:
-            self.init_command()
-
-        elif self.args['add']:
-            self.add_command()
-
-    def init_command(self, default_project=None):
-
-        if not self.args['PROJECT']:
-            self.args['PROJECT'] = self._prompt_init_project(default_project)
-
-        project = Project(template_dir=self.args['PROJECT'])
-        project.initialise(output_dir=self.args['--output'], variables={})
-        self.project = project
-
-    def add_command(self, default_template=None):
-
-        project_dir = Path(self.args['--output']).resolve()
-        mason_file = project_dir / '.mason'
-
-        if not mason_file.exists():
-            raise IOError(
-                f'A ".mason" file was not detected in the output directoty {project_dir}')
-
-        project = Project(mason_file=mason_file)
-
-        if not self.args['TEMPLATE']:
-            # inquire which template to use
-            self.args['TEMPLATE'] = self._prompt_add_template(project, default_template)
-
-        for template_name in self.args['TEMPLATE']:
-            project.add_template(template_name, variables={})
-
-        self.project = project
-
-    def _prompt_init_project(self, default=None):
-        """Return the choice of project to intialise"""
-
-        known_projects = list(self.config['known_projects'].keys())
-        known_projects.sort()
-
-        if not known_projects:
-            raise ValueError("No project specified and no recent projects to choose from.")
-
-        if default:
-            default_idx = known_projects.index(default)
-        else:
-            default_idx = 0
-
-        questions = [
-            inquirer.List('project',
-                          message="What project do you want to initialise?",
-                          choices=known_projects,
-                          default=default_idx)
-        ]
-        answers = inquirer.prompt(questions)
-
-        project_path = known_projects[answers['project']]
-
-        return project_path
-
-    def _prompt_add_template(self, project, default=None):
-        """Return the choice of templates to add"""
-
-        remaining_templates_names = project.remaining_templates
-
-        if default:
-            default_idx = known_projects.index(default)
-        else:
-            default_idx = 0
-
-        questions = [
-            inquirer.Checkbox('templates',
-                              message="What templates do you wish to add?",
-                              choices=remaining_templates_names,
-                              default=default_idx)
-        ]
-
-        answers = inquirer.prompt(questions)
-
-        templates = answers['templates']
-
-        return templates
 
 
 class MasonrySchema:
